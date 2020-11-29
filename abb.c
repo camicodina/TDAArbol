45bd8bcd57ed3a6d4c9d4a0f08310f5d9e9d6f3d
Camila-Codina <Camila.Codina@ibm.com>
2020-11-29 04:17:53 -0300

    funciones basicas

--- abb.c
+++ abb.c
@@ -7,37 +7,6 @@
 #include "abb.h"
 
 /*
- * Comparador de elementos. Recibe dos elementos del arbol y devuelve
- * 0 en caso de ser iguales, 1 si el primer elemento es mayor al
- * segundo o -1 si el primer elemento es menor al segundo.
- */
-typedef int (*abb_comparador)(void*, void*){
-    
-}
-
-/*
- * Destructor de elementos. Cada vez que un elemento deja el arbol
- * (arbol_borrar o arbol_destruir) se invoca al destructor pasandole
- * el elemento.
- */
-typedef void (*abb_liberar_elemento)(void*){
-
-}
-
-
-typedef struct nodo_abb {
-  void* elemento;
-  struct nodo_abb* izquierda;
-  struct nodo_abb* derecha;
-} nodo_abb_t;
-
-typedef struct abb{
-  nodo_abb_t* nodo_raiz;
-  abb_comparador comparador;
-  abb_liberar_elemento destructor;
-} abb_t;
-
-/*
  * Crea el arbol y reserva la memoria necesaria de la estructura.
  * Comparador se utiliza para comparar dos elementos.
  * Destructor es invocado sobre cada elemento que sale del arbol,
@@ -46,8 +15,11 @@ typedef struct abb{
  * Devuelve un puntero al arbol creado o NULL en caso de error.
  */
 abb_t* arbol_crear(abb_comparador comparador, abb_liberar_elemento destructor){
-    abb_t* arbol = calloc(1, sizeof(abb_t));
+    abb_t* arbol = malloc(sizeof(abb_t));
     if(!arbol) return NULL;
+    arbol->nodo_raiz=NULL;
+    arbol->comparador=comparador;
+    arbol->destructor=destructor;
     return arbol;
 }
 
@@ -76,6 +48,13 @@ int arbol_insertar(abb_t* arbol, void* elemento){
     int comparador = arbol->comparador(arbol->nodo_raiz->elemento, elemento);
     nodo_abb_t* nuevo_nodo = aux_crear_nodos(elemento);
     if(!nuevo_nodo) return -1;
+
+    if(!arbol->nodo_raiz){
+        arbol->nodo_raiz = nuevo_nodo;
+        return 0;
+    }
+    // nodo_abb_t* nodo_padre = arbol->nodo_raiz;
+    
     if((comparador == 0)||(comparador == 1)){ //primero mayor al segundo o son iguales, me muevo a la izquierda
         if(arbol->nodo_raiz->izquierda != NULL){
             free(nuevo_nodo);
@@ -93,6 +72,33 @@ int arbol_insertar(abb_t* arbol, void* elemento){
 }
 
 /*
+ * Para un elemento dado devuelve:
+ * - 0 si no tiene hijos o no existe el nodo
+ * - 1 si tiene un hijo
+ * - 2 si tiene dos hijos.
+ */
+int aux_cantidad_hijos(nodo_abb_t* nodo){
+	if((!nodo) || (!nodo->izquierda && !nodo->derecha)){
+        return 0;
+    }else if((nodo->derecha && !(nodo->izquierda)) || (!(nodo->derecha) && nodo->izquierda)){
+        return 1;
+    }else{ //nodo->derecha && nodo->izquierda
+        return 2;
+    }
+}
+
+/*
+ * Busca el menor elemento dentro del subarbol derecho
+ */
+nodo_abb_t* aux_buscar_menor_subarbol_derecho(nodo_abb_t* nodo_menor_subarbol){
+	if(!nodo_menor_subarbol->izquierda){
+        return nodo_menor_subarbol;
+    }else{
+        return aux_buscar_menor_subarbol_derecho(nodo_menor_subarbol->izquierda);
+    }
+}
+
+/*
  * Busca en el arbol un elemento igual al provisto (utilizando la
  * funcion de comparación) y si lo encuentra lo quita del arbol.
  * Adicionalmente, si encuentra el elemento, invoca el destructor con
@@ -100,8 +106,46 @@ int arbol_insertar(abb_t* arbol, void* elemento){
  * Devuelve 0 si pudo eliminar el elemento o -1 en caso contrario.
  */
 int arbol_borrar(abb_t* arbol, void* elemento){
+    if(!arbol) return -1;
+    if(!arbol->nodo_raiz) return -1;
+    if(!elemento) return -1;
+    if((!arbol_buscar(arbol,elemento)) || (arbol_buscar(arbol,elemento) != elemento)) return -1;
 
+    int comparador = arbol->comparador(arbol->nodo_raiz->elemento, elemento);
+    if((comparador == 0) && aux_cantidad_hijos(arbol->nodo_raiz) == 0){ // nodo_raiz tiene al elemento buscado y no tiene hijos
+        arbol->destructor(arbol->nodo_raiz->elemento);
+        arbol->nodo_raiz = NULL;
+        return 0;
+    }
+    if(comparador == 1){
+        if(arbol->nodo_raiz->izquierda){
+            arbol_borrar(arbol->nodo_raiz->izquierda, elemento);
+        }
+    }else if(comparador == -1){
+        if(arbol->nodo_raiz->derecha){
+            arbol_borrar(arbol->nodo_raiz->derecha, elemento);
+        }
+    }else{
+        if(aux_cantidad_hijos(arbol->nodo_raiz)==0){
+            arbol->destructor(arbol->nodo_raiz->elemento);
+            arbol->nodo_raiz = NULL;
+        }else if(aux_cantidad_hijos(arbol->nodo_raiz)==1){
+            if(!arbol->nodo_raiz->derecha){
+                arbol->nodo_raiz->elemento = arbol->nodo_raiz->izquierda->elemento;
+                arbol->nodo_raiz->izquierda = arbol_borrar(arbol->nodo_raiz->izquierda, arbol->nodo_raiz->elemento);
+            }else{ //no hay hijo izquierdo
+                arbol->nodo_raiz->elemento = arbol->nodo_raiz->derecha->elemento;
+                arbol->nodo_raiz->derecha = arbol_borrar(arbol->nodo_raiz->derecha, arbol->nodo_raiz->elemento);
+            }
+        }else{ //2 hijos
+            nodo_abb_t* menor_subarbol_derecho = aux_buscar_menor_subarbol_derecho(arbol->nodo_raiz->derecha);
+            arbol->nodo_raiz->elemento = menor_subarbol_derecho->elemento;
+            menor_subarbol_derecho = arbol_borrar(menor_subarbol_derecho, menor_subarbol_derecho->elemento);
+        }
+    }
+    return 0;
 }
+
 /*
  * Busca en el arbol un elemento igual al provisto (utilizando la
  * funcion de comparación).
@@ -110,6 +154,7 @@ int arbol_borrar(abb_t* arbol, void* elemento){
  */
 void* arbol_buscar(abb_t* arbol, void* elemento){
     if(!arbol) return NULL;
+    if(!arbol->nodo_raiz) return -1;
     if(!elemento) return NULL;
     int comparador = arbol->comparador(arbol->nodo_raiz, elemento);
     if(comparador == 1){ //me muevo a izquierda
@@ -133,7 +178,11 @@ void* arbol_buscar(abb_t* arbol, void* elemento){
  */
 void* arbol_raiz(abb_t* arbol){
     if(!arbol) return NULL;
-    return arbol->nodo_raiz->elemento;
+    if(arbol_vacio(arbol)==false){
+        return arbol->nodo_raiz->elemento;
+    }else{
+        return NULL;
+    }
 }
 
 /*
